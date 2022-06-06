4f684506e5b5087a3898267329480d63904316f8
moonkanghyuck <moonkanghyuck@gmail.com>
2022-06-06 23:07:43 +0900

    06/06_스켈레톤 AI 제작중

--- Assets/Scripts/Monster/MonsterBase.cs
+++ Assets/Scripts/Monster/MonsterBase.cs
@@ -99,6 +99,10 @@ public class MonsterBase : MonoBehaviour, IMonster
 	private float velocityChangeSpeed = 0.01f; //가변 증가값
 	[SerializeField]
 	private int _hp = 100; //체력
+	[SerializeField]
+	private float _atkRange = 1.5f; //몬스터 공격 거리
+	[SerializeField]
+	public GameObject _targetCharacter = null; //몬스터의 타겟
 
 	//참조하는 속성
 	private CharacterController _characterController = null;
@@ -111,6 +115,7 @@ public class MonsterBase : MonoBehaviour, IMonster
 	private Vector3 _moveDirect = Vector3.zero; //이동방향
 	private Vector3 _gravityDirect = Vector3.zero; //중력 벡터
 	private Vector3 currentVelocitySpeed = Vector3.zero; //캐릭터 현재 이동 속도
+	private Vector3 posTarget = Vector3.zero; //몬스터가 본 타겟 위치
 
 
 	private void Start()
@@ -124,7 +129,8 @@ public class MonsterBase : MonoBehaviour, IMonster
 	{
 		if(!_isCapture)
 		{
-			MonsterAI();
+			CheckState();
+			//MonsterAI();
 		}
 		else
 		{
@@ -133,6 +139,113 @@ public class MonsterBase : MonoBehaviour, IMonster
 		BodyDirectChange();
 		SetGravity();
 		SetAnimation();
+	}
+
+	/// <summary>
+	/// 해골 상태에 따라 동작 제어하는 함수
+	/// </summary>
+	void CheckState()
+	{
+		switch (_monsterState)
+		{
+			case MonsterState.Idle:
+				//setIdle(); // Idle -> GoTarget or Move
+				break;
+			case MonsterState.GoTarget:
+			case MonsterState.Move:
+				SetMove();
+				break;
+			case MonsterState.Attack:
+				//setAttack();
+				break;
+			case MonsterState.Damage:
+				break;
+			case MonsterState.Die:
+				break;
+			default:
+				break;
+		}
+	}
+
+	/// <summary>
+	/// 해골 상태가 Move혹은 GOtoTarget일 때 이동 함수
+	/// </summary>
+	void SetMove()
+	{
+		//출발점과 도착점 두 벡터의 차이
+		Vector3 distance = Vector3.zero;
+		//방향
+		Vector3 posLookAt = Vector3.zero;
+
+		switch (_monsterState)
+		{
+			case MonsterState.Move:
+				//목표 위치가 있을 때
+				if (posTarget != Vector3.zero)
+				{
+					//목표 위치와 해골 위치 차이 구하기
+					distance = posTarget - transform.position;
+
+					if (distance.magnitude < _atkRange)
+					{
+						StartCoroutine(SetWait());
+						return;
+					}
+
+					//바라보는 방향
+					posLookAt = new Vector3(posTarget.x, transform.position.y, posTarget.z);
+				}
+				break;
+			case MonsterState.GoTarget:
+				if (_targetCharacter != null)
+				{
+					//타겟 위치와 해골 위치 차이 구하기
+					distance = _targetCharacter.transform.position - transform.position;
+
+
+					if (distance.magnitude < _atkRange)
+					{
+						_monsterState = MonsterState.Attack;
+						return;
+					}
+
+					//바라보는 방향
+					posLookAt = new Vector3(_targetCharacter.transform.position.x, transform.position.y, _targetCharacter.transform.position.z);
+				}
+				break;
+		}
+
+		//해골이 이동할 방향은 크기가 없고 방향만 가빈 벡터
+		Vector3 direction = distance.normalized;
+
+		//방향은 x,z만 사용
+		direction = new Vector3(direction.x, 0f, direction.z);
+
+		//이동량 방향 구함
+		Vector3 amount = direction * _moveSpeed * Time.deltaTime;
+
+		//월드 좌표로 이동
+		_characterController.Move(amount);
+
+		//캐릭터 방향
+		transform.LookAt(posLookAt);
+
+	}
+
+	/// <summary>
+	/// 정지 상태 코루틴 함수
+	/// </summary>
+	/// <returns></returns>
+	IEnumerator SetWait()
+	{
+		//대기 상태로 변경
+		_monsterState = MonsterState.Wait;
+		//대기 시간 설정
+		float timeWait = Random.Range(1f, 3f);
+		//대기시간 작동
+		yield return new WaitForSeconds(timeWait);
+		//Idle로 돌려준다
+		_monsterState = MonsterState.Idle;
 	}
 
 	public bool CheckCapture(int level)
